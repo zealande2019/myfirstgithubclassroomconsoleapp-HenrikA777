cc6a9242c77d87d08a808e33b189532bb6b5bcd2
Henrik Andersen <henr563a@edu.easj.dk>
2019-02-01 11:08:37 +0100

    ABC

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        public Demo()
+        {
+            
+        }
+
+        public int Value { get; set; }
+
+        public string Navn{ get; private set; }
+    }
+}
